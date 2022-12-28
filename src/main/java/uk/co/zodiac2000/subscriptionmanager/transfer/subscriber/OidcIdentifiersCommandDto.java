44ea527841edc6a872d1bd0b55321c53cd6bc749
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2022-12-28 19:49:56 +0000

    #10 javadoc improvements

--- src/main/java/uk/co/zodiac2000/subscriptionmanager/transfer/subscriber/OidcIdentifiersCommandDto.java
+++ src/main/java/uk/co/zodiac2000/subscriptionmanager/transfer/subscriber/OidcIdentifiersCommandDto.java
@@ -4,26 +4,28 @@ import java.util.List;
 import javax.validation.Valid;
 
 /**
- * Command DTO that has a reference to a set of OidcIdentifierCommandDto objects. The main purpose of this
- * class is to allow the members of the oidcIdentifierCommandDtos collection to be validated.
+ * Command DTO that holds a reference to a list of {@code OidcIdentifierCommandDto} objects. The main purpose of this
+ * class is to allow the members of the {@code oidcIdentifiers} collection to be validated.
  */
 public class OidcIdentifiersCommandDto {
 
     @Valid
     private List<OidcIdentifierCommandDto> oidcIdentifiers;
 
+    /**
+     * Zero-arg constructor.
+     */
     public OidcIdentifiersCommandDto() { }
 
     /**
-     *
-     * @param oidcIdentifiers
+     * @param oidcIdentifiers a list of OidcIdentifierCommandDto objects
      */
     public OidcIdentifiersCommandDto(List<OidcIdentifierCommandDto> oidcIdentifiers) {
         this.oidcIdentifiers = oidcIdentifiers;
     }
 
     /**
-     * @return the oidcIdentifiers
+     * @return a list of OidcIdentifierCommandDto objects
      */
     public List<OidcIdentifierCommandDto> getOidcIdentifiers() {
         return oidcIdentifiers;
