1389071fb1279b6c3d1635dcaf77ba6acbb752e3
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-01-05 16:01:03 +0000

    #27 added properties
    New properties: suspended, canBeSuspended, canBeTerminated, canBeTerminated

--- src/test/java/uk/co/zodiac2000/subscriptionmanager/transfer/subscription/SubscriptionResponseDtoTest.java
+++ src/test/java/uk/co/zodiac2000/subscriptionmanager/transfer/subscription/SubscriptionResponseDtoTest.java
@@ -14,24 +14,35 @@ public class SubscriptionResponseDtoTest {
     private static final Optional<LocalDate> START_DATE = Optional.of(LocalDate.parse("2021-01-01"));
     private static final Optional<LocalDate> END_DATE = Optional.of(LocalDate.parse("2022-01-01"));
     private static final boolean TERMINATED = false;
+    private static final boolean SUSPENDED = true;
     private static final String CONTENT_IDENTIFIER = "CONTENT";
     private static final Long SUBSCRIBER_ID = 87L;
-    private static final boolean ACTIVE = true;
+    private static final boolean ACTIVE = false;
+    private static final boolean CAN_BE_SUSPENDED = false;
+    private static final boolean CAN_BE_TERMINATED = true;
+    private static final boolean CAN_BE_UNSUSPENDED = true;
 
     /**
      * Test constructor and accessors.
      */
     @Test
     public void testAccessors() {
-        SubscriptionResponseDto responseDto = new SubscriptionResponseDto(ID, START_DATE, END_DATE, TERMINATED, CONTENT_IDENTIFIER,
-                SUBSCRIBER_ID, ACTIVE);
+        SubscriptionResponseDto responseDto = new SubscriptionResponseDto(ID, START_DATE, END_DATE,
+                TERMINATED, SUSPENDED,
+                CONTENT_IDENTIFIER, SUBSCRIBER_ID, ACTIVE,
+                CAN_BE_SUSPENDED, CAN_BE_TERMINATED, CAN_BE_UNSUSPENDED
+        );
 
         Assert.assertEquals(responseDto.getId(), ID);
         Assert.assertEquals(responseDto.getStartDate(), START_DATE);
         Assert.assertEquals(responseDto.getEndDate(), END_DATE);
         Assert.assertFalse(responseDto.isTerminated());
+        Assert.assertTrue(responseDto.isSuspended());
         Assert.assertEquals(responseDto.getContentIdentifier(), CONTENT_IDENTIFIER);
         Assert.assertEquals(responseDto.getSubscriberId(), SUBSCRIBER_ID);
-        Assert.assertTrue(responseDto.isActive());
+        Assert.assertFalse(responseDto.isActive());
+        Assert.assertFalse(responseDto.getCanBeSuspended());
+        Assert.assertTrue(responseDto.getCanBeTerminated());
+        Assert.assertTrue(responseDto.getCanBeUnsuspended());
     }
 }
