65084e20d8b3f1f76af1760325c0396507f61bb5
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-01-16 09:52:17 +0000

    #30 corrected unique index name

--- src/main/resources/db/changelog/sql/00060-claim-name.sql
+++ src/main/resources/db/changelog/sql/00060-claim-name.sql
@@ -7,4 +7,4 @@ CREATE TABLE claim_name (
     claim_name VARCHAR(1000) NOT NULL
 );
 
-CREATE UNIQUE INDEX claim_name_uq_subscriber_id ON claim_name (claim_name);
+CREATE UNIQUE INDEX claim_name_uq_claim_name ON claim_name (claim_name);
