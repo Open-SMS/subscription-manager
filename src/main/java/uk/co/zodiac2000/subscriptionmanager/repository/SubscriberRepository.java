bab9eb1c726d13af68903c225bd1074c3e429a25
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-01-24 14:54:38 +0000

    #30 removed findByOidcIdentifiersIssuerAndOidcIdentifiersSubject
    No longer required

--- src/main/java/uk/co/zodiac2000/subscriptionmanager/repository/SubscriberRepository.java
+++ src/main/java/uk/co/zodiac2000/subscriptionmanager/repository/SubscriberRepository.java
@@ -11,14 +11,6 @@ import uk.co.zodiac2000.subscriptionmanager.domain.subscriber.Subscriber;
 public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
 
     /**
-     * Returns subscribers associated with at least one OidcIdentifier matching the arguments.
-     * @param issuer the iss claim
-     * @param subject the sub claim
-     * @return a set of identified subscribers
-     */
-    Set<Subscriber> findByOidcIdentifiersIssuerAndOidcIdentifiersSubject(String issuer, String subject);
-
-    /**
      * Returns subscribers associated with at least one SamlIdentifier matching the arguments.
      * @param entityId the issuer's entityId
      * @param scopedAffiliation the value of the eduPersonScopedAffiliation attribute released by the issuer
