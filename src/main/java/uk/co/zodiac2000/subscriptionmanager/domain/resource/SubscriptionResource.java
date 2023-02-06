035c0cd2ca94f4a5b27a2d5065dc9bed7946c6a7
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-02-06 14:36:34 +0000

    #45 SubscriptionResource aggregate initial implementation

---
+++ src/main/java/uk/co/zodiac2000/subscriptionmanager/domain/resource/SubscriptionResource.java
@@ -0,0 +1,72 @@
+package uk.co.zodiac2000.subscriptionmanager.domain.resource;
+
+import java.io.Serializable;
+import java.net.URI;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * Class representing a subscribeable resource, for example a website that provides content available to subscribers.
+ * The resource is uniquely identified by {@code resourceUri}. This may be a URL identifying a website or part of a
+ * website, or any other URI that is used to identify a resource in an authorization request.
+ */
+@Entity
+public class SubscriptionResource implements Serializable {
+
+    private static final long serialVersionUID = 21412L;
+    private static final int MAX_RESOURCE_URI_LENGTH = 1000;
+    private static final int MAX_RESOURCE_DESCRIPTION_LENGTH = 1000;
+
+    @Id
+    @SequenceGenerator(name = "subscription_id_gen", sequenceName = "subscription_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "subscription_id_gen")
+    private Long id;
+
+    @NotEmpty
+    @Length(max = MAX_RESOURCE_URI_LENGTH)
+    private URI resourceUri;
+
+    @NotEmpty
+    @Length(max = MAX_RESOURCE_DESCRIPTION_LENGTH)
+    private String resourceDescription;
+
+    /**
+     * Zero-arg constructor for JPA.
+     */
+    public SubscriptionResource() { }
+
+    /**
+     * Constructs a new Resource using the supplied arguments.
+     * @param resourceUri the resource URI
+     * @param resourceDescription the resource description
+     */
+    public SubscriptionResource(final URI resourceUri, final String resourceDescription) {
+        this.resourceUri = resourceUri;
+        this.resourceDescription = resourceDescription;
+    }
+
+    /**
+     * @return the resource id
+     */
+    public Long getId() {
+        return this.id;
+    }
+
+    /**
+     * @return the resource URI
+     */
+    public URI getResourceUri() {
+        return this.resourceUri;
+    }
+
+    /**
+     * @return the resource description
+     */
+    public String getResourceDescription() {
+        return this.resourceDescription;
+    }
+}
---
+++ src/test/java/uk/co/zodiac2000/subscriptionmanager/domain/resource/SubscriptionResourceTest.java
@@ -0,0 +1,36 @@
+package uk.co.zodiac2000.subscriptionmanager.domain.resource;
+
+import java.net.URI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests for SubscriptionResource.
+ */
+public class SubscriptionResourceTest {
+
+    private static final URI RESOURCE_URI = URI.create("https://example.com");
+    private static final String RESOURCE_DESCRIPTION = "Example";
+
+    /**
+     * Test constructor and accessors.
+     */
+    @Test
+    public void testConstructor() {
+        SubscriptionResource resource = new SubscriptionResource(RESOURCE_URI, RESOURCE_DESCRIPTION);
+
+        Assert.assertNull(resource.getId());
+        Assert.assertEquals(resource.getResourceUri(), RESOURCE_URI);
+        Assert.assertEquals(resource.getResourceDescription(), RESOURCE_DESCRIPTION);
+    }
+
+    /**
+     * Test zero-arg constructor.
+     */
+    @Test
+    public void testZeroArgConstructor() {
+        SubscriptionResource resource = new SubscriptionResource();
+
+        Assert.assertNotNull(resource);
+    }
+}
