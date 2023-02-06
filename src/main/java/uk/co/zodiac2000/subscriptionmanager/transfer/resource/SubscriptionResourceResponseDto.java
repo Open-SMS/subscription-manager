6748eff06d288346e94f91b13ddf0bfc3263f6cd
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-02-06 15:46:04 +0000

    #45 response DTO representing a subscription resource

---
+++ src/main/java/uk/co/zodiac2000/subscriptionmanager/transfer/resource/SubscriptionResourceResponseDto.java
@@ -0,0 +1,46 @@
+package uk.co.zodiac2000.subscriptionmanager.transfer.resource;
+
+/**
+ * Response DTO representing a subscription resource.
+ */
+public class SubscriptionResourceResponseDto {
+
+    private final long id;
+
+    private final String resourceUri;
+
+    private final String resourceDescription;
+
+    /**
+     * Constructs a new SubscriptionResourceResponseDto with the supplied arguments.
+     * @param id the subscription resource id
+     * @param resourceUri the resource URI
+     * @param resourceDescription the resource description
+     */
+    public SubscriptionResourceResponseDto(long id, String resourceUri, String resourceDescription) {
+        this.id = id;
+        this.resourceUri = resourceUri;
+        this.resourceDescription = resourceDescription;
+    }
+
+    /**
+     * @return the subscription resource id
+     */
+    public long getId() {
+        return this.id;
+    }
+
+    /**
+     * @return the resource URI as a string
+     */
+    public String getResourceUri() {
+        return this.resourceUri;
+    }
+
+    /**
+     * @return the resource description
+     */
+    public String getResourceDescription() {
+        return this.resourceDescription;
+    }
+}
