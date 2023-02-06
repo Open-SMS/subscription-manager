98bf63ed94da5ba2e7342d00423851921a2bd8f7
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-02-06 16:45:53 +0000

    #45 corrected SQL

--- src/main/resources/db/changelog/sql/00100-content-identifier.sql
+++ src/main/resources/db/changelog/sql/00100-content-identifier.sql
@@ -6,7 +6,7 @@ CREATE TABLE content_identifier (
     id SERIAL PRIMARY KEY,
     subscription_resource_id BIGINT NOT NULL,
     CONSTRAINT content_identifier_fk_subscription_resource_id
-        FOREIGN KEY subscription_resource_id REFERENCES subscription_resource (id)
+        FOREIGN KEY (subscription_resource_id) REFERENCES subscription_resource (id)
 );
 
 CREATE INDEX content_identifier_ix_subscription_resource_id ON content_identifier (subscription_resource_id);
