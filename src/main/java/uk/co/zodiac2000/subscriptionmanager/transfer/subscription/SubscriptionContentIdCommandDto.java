8b67a8c940e5429f62f92683a0433760c1fe8923
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-02-10 15:24:45 +0000

    #45 now specifies subscription content by reference

---
+++ src/main/java/uk/co/zodiac2000/subscriptionmanager/transfer/subscription/SubscriptionContentIdCommandDto.java
@@ -0,0 +1,36 @@
+package uk.co.zodiac2000.subscriptionmanager.transfer.subscription;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import uk.co.zodiac2000.subscriptionmanager.ApplicationConstants;
+
+/**
+ * Command DTO representing subscription content to be associated with a subscription.
+ */
+public class SubscriptionContentIdCommandDto {
+
+    @NotNull
+    @Digits(integer = ApplicationConstants.MAX_LONG_DIGITS, fraction = 0)
+    private String subscriptionContentId;
+
+    /**
+     * Zero-arg constructor to allow ObjectMapper to create this class.
+     */
+    public SubscriptionContentIdCommandDto() { }
+
+    /**
+     * Constructs a new SubscriptionContentIdCommandDto using the supplied argument.
+     * @param subscriptionContentId the identifier of the subscription content that this subscription
+     * provides access to
+     */
+    public SubscriptionContentIdCommandDto(final String subscriptionContentId) {
+        this.subscriptionContentId = subscriptionContentId;
+    }
+
+    /**
+     * @return the identifier of the subscription content that this subscription provides access to
+     */
+    public String getSubscriptionContentId() {
+        return this.subscriptionContentId;
+    }
+}
