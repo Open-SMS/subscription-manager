73d011197835f347842bd7b3f5400cd5567cecaa
Simon Rees <29239440+simon-rees@users.noreply.github.com>
2023-03-07 12:50:33 +0000

    #45 implemented createSubscriptionContent

--- src/main/java/uk/co/zodiac2000/subscriptionmanager/api/SubscriptionContentController.java
+++ src/main/java/uk/co/zodiac2000/subscriptionmanager/api/SubscriptionContentController.java
@@ -1,12 +1,17 @@
 package uk.co.zodiac2000.subscriptionmanager.api;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.co.zodiac2000.subscriptionmanager.service.SubscriptionContentService;
+import uk.co.zodiac2000.subscriptionmanager.transfer.subscriptioncontent.NewSubscriptionContentCommandDto;
 import uk.co.zodiac2000.subscriptionmanager.transfer.subscriptioncontent.SubscriptionContentResponseDto;
 
 /**
@@ -27,5 +32,18 @@ public class SubscriptionContentController {
     @GetMapping(path = "/{id}")
     public ResponseEntity<SubscriptionContentResponseDto> getSubscriptionContent(@PathVariable("id") final long id) {
         return ResponseEntity.of(this.subscriptionContentService.getSubscriptionContent(id));
+    }
+
+    /**
+     * Creates a new SubscriptionContent aggregate root and persists it. Returns a response document representing the
+     * new subscription content.
+     * @param commandDto command DTO representing the new subscription content
+     * @return a SubscriptionContentResponseDto
+     */
+    @PostMapping
+    public ResponseEntity<SubscriptionContentResponseDto> createSubscriptionContent(
+            @RequestBody @Valid final NewSubscriptionContentCommandDto commandDto) {
+        return new ResponseEntity<>(this.subscriptionContentService.createSubscriptionContent(commandDto).get(),
+                HttpStatus.CREATED);
     }
 }
--- src/test/java/uk/co/zodiac2000/subscriptionmanager/api/SubscriptionContentControllerTestITCase.java
+++ src/test/java/uk/co/zodiac2000/subscriptionmanager/api/SubscriptionContentControllerTestITCase.java
@@ -1,17 +1,28 @@
 package uk.co.zodiac2000.subscriptionmanager.api;
 
+import com.jayway.jsonpath.JsonPath;
+import java.util.Optional;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import uk.co.zodiac2000.subscriptionmanager.configuration.TestClockConfiguration;
+import uk.co.zodiac2000.subscriptionmanager.domain.subscriptioncontent.SubscriptionContent;
+import uk.co.zodiac2000.subscriptionmanager.repository.SubscriptionContentRepository;
 
 /**
  * Integration tests for SubscriptionContentController.  These tests use MockMvc to test the controller without starting
@@ -21,6 +32,9 @@ import uk.co.zodiac2000.subscriptionmanager.configuration.TestClockConfiguration
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-integration.properties")
 public class SubscriptionContentControllerTestITCase extends AbstractTransactionalTestNGSpringContextTests {
+
+    @Autowired
+    private SubscriptionContentRepository subscriptionContentRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -67,5 +81,109 @@ public class SubscriptionContentControllerTestITCase extends AbstractTransaction
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isNotFound();
+    }
+
+    /**
+     * Test createSubscriptionContent.
+     */
+    @Test
+    public void testCreateSubscriptionContent() {
+        String newSubscriptionContentJsonn = "{"
+                + " \"contentDescription\":\"Test content\","
+                + " \"subscriptionResourceId\":\"100000001\","
+                + " \"contentIdentifiers\":["
+                + "   {\"contentIdentifier\":\"TEST-CONTENT-ITEM-ONE\"},"
+                + "   {\"contentIdentifier\":\"TEST-CONTENT-ITEM-TWO\"}"
+                + " ]"
+                + "}";
+        EntityExchangeResult<byte[]> result = this.client.post().uri("/subscription-content")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(newSubscriptionContentJsonn)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBody().returnResult();
+        this.subscriptionContentRepository.flush();
+
+        String responseBody = new String(result.getResponseBody());
+        Integer id = JsonPath.read(responseBody, "$.id");
+        Assert.assertNotNull(id);
+
+        Optional<SubscriptionContent> subscriptionContent = this.subscriptionContentRepository.findById(id.longValue());
+        Assert.assertTrue(subscriptionContent.isPresent());
+        assertThat(subscriptionContent.get(), allOf(
+                hasProperty("contentDescription", is("Test content")),
+                hasProperty("subscriptionResourceId", is(100000001L)),
+                hasProperty("contentIdentifiers", containsInAnyOrder(
+                        hasProperty("contentIdentifier", is("TEST-CONTENT-ITEM-ONE")),
+                        hasProperty("contentIdentifier", is("TEST-CONTENT-ITEM-TWO"))
+                ))
+        ));
+    }
+
+    /**
+     * Test createSubscriptionContent when subscriptionResourceId is not an integer.
+     */
+    @Test
+    public void testCreateSubscriptionContentSubscriptionResourceIdNotInteger() {
+        String newSubscriptionContentJsonn = "{"
+                + " \"contentDescription\":\"Test content\","
+                + " \"subscriptionResourceId\":\"foo\","
+                + " \"contentIdentifiers\":["
+                + "   {\"contentIdentifier\":\"TEST-CONTENT-ITEM-ONE\"},"
+                + "   {\"contentIdentifier\":\"TEST-CONTENT-ITEM-TWO\"}"
+                + " ]"
+                + "}";
+        this.client.post().uri("/subscription-content")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(newSubscriptionContentJsonn)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .consumeWith(res -> {
+                    BindException ex = (BindException) ((MvcResult) res.getMockServerResult()).getResolvedException();
+                    Assert.assertNotNull(ex);
+                    Assert.assertEquals(ex.getErrorCount(), 1);
+                    FieldError error = ex.getFieldError();
+                    Assert.assertNotNull(error);
+                    Assert.assertEquals(error.getField(), "subscriptionResourceId");
+                    Assert.assertEquals(error.getDefaultMessage(),
+                            "numeric value out of bounds (<18 digits>.<0 digits> expected)");
+                });
+    }
+
+    /**
+     * Test createSubscriptionContent when subscriptionResourceId does not reference a subscription resource
+     * that exists in the system.
+     */
+    @Test
+    public void testCreateSubscriptionContentSubscriptionResourceIdNotExists() {
+        String newSubscriptionContentJsonn = "{"
+                + " \"contentDescription\":\"Test content\","
+                + " \"subscriptionResourceId\":\"42\","
+                + " \"contentIdentifiers\":["
+                + "   {\"contentIdentifier\":\"TEST-CONTENT-ITEM-ONE\"},"
+                + "   {\"contentIdentifier\":\"TEST-CONTENT-ITEM-TWO\"}"
+                + " ]"
+                + "}";
+        this.client.post().uri("/subscription-content")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(newSubscriptionContentJsonn)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .consumeWith(res -> {
+                    BindException ex = (BindException) ((MvcResult) res.getMockServerResult()).getResolvedException();
+                    Assert.assertNotNull(ex);
+                    Assert.assertEquals(ex.getErrorCount(), 1);
+                    FieldError error = ex.getFieldError();
+                    Assert.assertNotNull(error);
+                    Assert.assertEquals(error.getField(), "subscriptionResourceId");
+                    Assert.assertEquals(error.getDefaultMessage(),
+                            "{uk.co.zodiac2000.subscriptionmanager.constraint.Exists}");
+                });
     }
 }
